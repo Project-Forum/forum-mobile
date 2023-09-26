2ca812959b734d1186251ef7745b7311f4088664
fadillaoegi <fadilfdl2710@gmail.com>
2023-09-26 16:19:19 +0700

    Update

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:romusa/styles/colors.dart';
+import 'package:romusa/styles/fonts.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
@@ -7,9 +9,127 @@ class SignUpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: MediaQuery.sizeOf(context).width,
         margin: const EdgeInsets.all(28.0),
-        child: const Column(
-          children: [Text("Sign Up")],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 28.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // NOTE: BUTTON BACK
+                GestureDetector(
+                  onTap: () {},
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.arrow_back_ios,
+                        color: RomusaColor.colorPrimary,
+                      ),
+                      Text(
+                        "Kembali",
+                        style: fontPrimary700.copyWith(fontSize: 17.0),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // NOTE: BUTTON ICON
+                GestureDetector(
+                  onTap: () {},
+                  child: SizedBox(
+                    height: 32.0,
+                    width: 32.0,
+                    child: CircleAvatar(
+                        backgroundColor: RomusaColor.colorPrimary,
+                        child: const Icon(
+                          Icons.question_mark_rounded,
+                        )),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+            Text(
+              "Masukkan E-mail",
+              style: fontBlack700.copyWith(fontSize: 24.0),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              "Masukkan e-mail aktif kamu untuk\nmelakukan pendaftaran",
+              style: fontBlack600.copyWith(fontSize: 17.0),
+            ),
+            const SizedBox(
+              height: 44.0,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        width: 2.0, color: RomusaColor.colorDisable)),
+              ),
+            ),
+            const SizedBox(
+              height: 44.0,
+            ),
+            SizedBox(
+                width: double.infinity,
+                height: 55.0,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: RomusaColor.colorPrimary),
+                  child: Text(
+                    "Masuk",
+                    style: fontWhite700.copyWith(fontSize: 17.0),
+                  ),
+                )),
+            const SizedBox(
+              height: 44.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Belum punya akun? ",
+                  style: fontBlack500.copyWith(fontSize: 17.0),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Daftar Sekarang",
+                    style: fontPrimary700.copyWith(fontSize: 17.0),
+                  ),
+                )
+              ],
+            ),
+
+            // NOTE: BREAK LINE
+            const Divider(
+              thickness: 1,
+              color: Colors.amber,
+            ),
+            const SizedBox(
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Divider(
+                    thickness: 1,
+                    color: Colors.amber,
+                  ),
+                  Text("Atau"),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/styles/fonts.dart
+++ lib/styles/fonts.dart
@@ -20,7 +20,7 @@ TextStyle fontBlack300 = GoogleFonts.openSans(
 TextStyle fontBlack400 = GoogleFonts.openSans(
     color: RomusaColor.colorBlack, fontWeight: FontWeight.w400);
 TextStyle fontBlack500 = GoogleFonts.openSans(
-    color: RomusaColor.colorBackground, fontWeight: FontWeight.w500);
+    color: RomusaColor.colorBlack, fontWeight: FontWeight.w500);
 TextStyle fontBlack600 = GoogleFonts.openSans(
     color: RomusaColor.colorBlack, fontWeight: FontWeight.w600);
 TextStyle fontBlack700 = GoogleFonts.openSans(
@@ -30,8 +30,14 @@ TextStyle fontBlack700 = GoogleFonts.openSans(
 TextStyle fontDisable300 = GoogleFonts.openSans(
     color: RomusaColor.colorDisable, fontWeight: FontWeight.w300);
 
+// NOTE: FONT DISABLE
+TextStyle fontPrimary700 = GoogleFonts.openSans(
+    color: RomusaColor.colorPrimary, fontWeight: FontWeight.w700);
+
 // NOTE: FONT GREY
 TextStyle fontGrey300 = GoogleFonts.openSans(
     color: RomusaColor.colorSecondary, fontWeight: FontWeight.w300);
 TextStyle fontGrey400 = GoogleFonts.openSans(
     color: RomusaColor.colorSecondary, fontWeight: FontWeight.w400);
+TextStyle fontGrey700 = GoogleFonts.openSans(
+    color: RomusaColor.colorSecondary, fontWeight: FontWeight.w700);
